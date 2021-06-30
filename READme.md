eafabb992fc155631756c48343eee737d21c8652
wleong02 <84175408+wleong02@users.noreply.github.com>
2021-06-30 19:17:38 +0800

    Upload files

---
+++ READme.md
@@ -0,0 +1,34 @@
+---
+editor_options: 
+  markdown: 
+    wrap: 72
+---
+
+**Getting and Cleaning Data**
+
+Course Project: Tidying Data
+
+User: wleong02
+
+**Overview**
+
+The "run_analysis.R" script should be in a directory that also has the
+Samsung data within (see
+<https://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones>
+for more information).
+
+**The script performs the following:**
+
+1\. Merges the training and test sets to create one data set
+
+2\. Appropriately labels the data set with descriptive variable names
+(refer to CodeBook.md)
+
+3\. Extracts only the variables that are mean and standard deviation for
+each measurement
+
+4\. Applies descriptive activity names to name the activities in the
+data set
+
+5\. Creates a second, independent tidy data set with the average of each
+variable for each activity and each subject (summarytidydata.txt)
---
+++ run_analysis.R
@@ -0,0 +1,51 @@
+# Getting and Cleaning Data Course Project
+
+if (!file.exists("Course Project")) {dir.create("Course Project")}
+
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
+              './Course Project/data.zip', mode = "wb")
+unzip('./Course Project/data.zip', exdir = 'Course Project')
+
+# Merge training and test sets
+library(readr)
+trainingset <- read.table('./Course Project/UCI HAR Dataset/train/X_train.txt')
+traininglabel <- read.table('./Course Project/UCI HAR Dataset/train/y_train.txt')
+trainingsubject <- read.table('./Course Project/UCI HAR Dataset/train/subject_train.txt')
+training <- cbind(trainingset, trainingsubject, traininglabel) # combine training set and label
+
+testset <- read.table('./Course Project/UCI HAR Dataset/test/X_test.txt')
+testlabel <- read.table('./Course Project/UCI HAR Dataset/test/y_test.txt')
+testsubject <- read.table('./Course Project/UCI HAR Dataset/test/subject_test.txt')
+test <- cbind(testset, testsubject, testlabel) # combine test set and label
+
+fulldata <- rbind(training, test) # combine training and test data
+
+# Label columns with feature names, subject, and label (last two column)
+featurenames <- read.table('./Course Project/UCI HAR Dataset/features.txt')
+varnames <- c(featurenames$V2, "subject", "label")
+colnames(fulldata) <- varnames
+
+# Make features names more readable/intuitive
+names(fulldata) <-gsub("^t", "time", names(fulldata))
+names(fulldata) <-gsub("^f", "freq", names(fulldata))
+names(fulldata) <-gsub("Acc", "Accelerometer", names(fulldata))
+names(fulldata) <-gsub("Gyro", "Gyroscope", names(fulldata))
+names(fulldata) <-gsub("Mag", "Magnitude", names(fulldata))
+names(fulldata) <-gsub("BodyBody", "Body", names(fulldata))
+ 
+# Extract only columns that are measurements of mean and standard deviation, and retain subject and label column
+subset <- fulldata[, grep("mean|std|subject|label", names(fulldata))]
+
+# Relabel activities with descriptive activity names
+activities <- read.table('./Course Project/UCI HAR Dataset/activity_labels.txt')
+colnames(activities) <- c('label','activity')
+tidydata <- merge(subset, activities, by = "label")
+
+# Create second, independent tidy data set with avg of each variable for each activity and each subject
+library(dplyr)
+
+summary <- tidydata %>%
+            group_by(activity, subject) %>%
+            summarize_all(mean, na.rm = TRUE)
+
+write.table(summary, "./Course Project/summarytidydata.txt", row.name = FALSE)
